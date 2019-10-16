0ca15d0233489961e4c9a12f34290771c3c4f014
Catli <10312@vtsd.com>
2019-10-16 08:14:06 -0400

    sorry :)

--- Lab05vst.java
+++ Lab05vst.java
@@ -17,5 +17,23 @@ public class Lab05vst extends Applet
 		int x2 = 990;
 		int y2 = 640;
 		g.drawRect(10,10,width,height);
+		for (int k = 0; k<=70; k++){
+			g.drawLine(990-(14*k),640,990,((9*k)+10));
+		}
+		for (int k = 0; k<=70; k++){
+			g.drawLine(10+(14*k),640,10,((9*k)+10));
+		}
+		for (int k = 0; k<=70; k++){
+			g.drawLine(990,640-(9*k),990-(14*k),10);
+		}
+		for (int k = 0; k<=70; k++){
+			g.drawLine(10,640-(9*k),10+(14*k),10);
+		}
+		g.drawRect(228,184,540,282);
+		//next
+		for (int k = 0; k<=68; k++){
+			g.drawLine(768-(8*k),466,768,184+(4*k));
+		}
+		g.drawLine(282,466,768,460);
+		}
 	}
-}
